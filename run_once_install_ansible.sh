21d6b0f672261ea853adcb39c4f7820b2d2fbf87
Alvaro <ninobaldo@gmail.com>
2024-03-09 17:15:43 -0300

    Install ansible and inicial setup

--- run_once_install_ansible.sh
+++ run_once_install_ansible.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+DOTFILES_DIR="$HOME/.dotfiles"
+
+
 if ! [ -x "$(command -v ansible)" ]; then
   sudo pacman -S ansible --noconfirm
 fi
 
+if [[ -f "$DOTFILES_DIR/requirements.yml" ]]; then
+  cd "$DOTFILES_DIR"
 
-# ansible-playbook ~/.bootstrap/setup.yml --ask-become-pass
+  ansible-galaxy install -r requirements.yml
+fi
+
+ansible-playbook "$DOTFILES_DIR/setup.yml" --ask-become-pass
 
 echo "Ansible installation complete."
 
