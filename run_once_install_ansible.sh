926f9a8df5706756780a287f49151c0a3c0afc5f
Alvaro <ninobaldo@gmail.com>
2024-09-01 07:33:02 -0300

    Add .tmux.conf Update install_ansible.sh

--- run_once_install_ansible.sh
+++ run_once_install_ansible.sh
@@ -7,13 +7,13 @@ if ! [ -x "$(command -v ansible)" ]; then
   sudo pacman -S ansible --noconfirm
 fi
 
-if [[ -f "$DOTFILES_DIR/requirements.yml" ]]; then
-  cd "$DOTFILES_DIR"
+# if [[ -f "$DOTFILES_DIR/requirements.yml" ]]; then
+#   cd "$DOTFILES_DIR"
+#
+#   ansible-galaxy install -r requirements.yml
+# fi
 
-  ansible-galaxy install -r requirements.yml
-fi
-
-ansible-playbook "$DOTFILES_DIR/setup.yml" --ask-become-pass
+# ansible-playbook "$DOTFILES_DIR/setup.yml" --ask-become-pass
 
 echo "Ansible installation complete."
 
